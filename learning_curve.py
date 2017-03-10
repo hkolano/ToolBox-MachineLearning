e2ee954e880652553ab40e635d87997050ce66be
hkolano <hannah.kolano@students.olin.edu>
2017-03-09 22:48:54 -0500

    plt.show doesn't show anything nor show any errors :(

--- learning_curve.py
+++ learning_curve.py
@@ -9,7 +9,6 @@ from sklearn.linear_model import LogisticRegression
 
 def display_digits():
     digits = load_digits()
-    print(digits.DESCR)
     fig = plt.figure()
     for i in range(10):
         subplot = fig.add_subplot(5, 2, i+1)
@@ -20,27 +19,29 @@ def display_digits():
 
 def train_model():
     data = load_digits()
+    model = LogisticRegression(C=10**-10)
     num_trials = 10
-    train_percentages = range(5, 95, 5)
+    train_percentages = [.05, .1, .15, .2, .25, .3, .35, .4, .45, .5, .55, .6,
+                         .65, .7, .75, .8, .85, .9, .95]
     test_accuracies = numpy.zeros(len(train_percentages))
+    for j in range(len(train_percentages)):
+        train_accuracy = []
+        test_accuracy = []
+        for i in range(num_trials):
+            X_train, X_test, y_train, y_test = train_test_split(data.data,
+                                                                data.target,
+                                                                train_size=train_percentages[j])
+            model.fit(X_train, y_train)
+            test_accuracy.append(model.score(X_test, y_test))
+    test_accuracies[j] = numpy.mean(test_accuracy)
 
-    # train models with training percentages between 5 and 90 (see
-    # train_percentages) and evaluate the resultant accuracy for each.
-    # You should repeat each training percentage num_trials times to smooth out
-    # variability.
-    # For consistency with the previous example use
-    # model = LogisticRegression(C=10**-10) for your learner
-
-    # TODO: your code here
-
-    fig = plt.figure()
     plt.plot(train_percentages, test_accuracies)
     plt.xlabel('Percentage of Data Used for Training')
     plt.ylabel('Accuracy on Test Set')
+    print("I did get here...")
     plt.show()
 
 
 if __name__ == "__main__":
-    # Feel free to comment/uncomment as needed
     display_digits()
     # train_model()
